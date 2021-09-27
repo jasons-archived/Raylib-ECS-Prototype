f559aa1d6cfee7a160a9572a1fdf7c3b71d0241d
JasonS <jasons@novaleaf.com>
2021-09-27 13:56:10 -0700

    add SimplePubSib and some diagnostics code

---
+++ DumDum/Bcl/Messaging/Channel.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DumDum.Bcl.Messaging;
+/// <summary>
+/// Represents a channel of communication between Publisher(s) and Subscriber(s).   
+/// Only one kind of message may be sent per Channel.
+/// </summary>
+/// <typeparam name="TMessage">must be struct (to prevent GC allocations).  Pass a tuple if you want to pass an object</typeparam>
+public class Channel<TMessage> where TMessage : struct
+{
+	private ReaderWriterLockSlim rwLock = new ReaderWriterLockSlim();
+
+	private List<WeakReference<ConcurrentQueue<TMessage>>> _subscribers = new List<WeakReference<ConcurrentQueue<TMessage>>>();
+
+	/// <summary>
+	/// key used to identify this channel
+	/// </summary>
+	public readonly string key;
+
+	private List<WeakReference<ConcurrentQueue<TMessage>>> GetSubscribers()
+	{
+		//Interlocked.MemoryBarrier(); //because we may lock if we use ConcurrentBag instead of a list. becuase concurrentbag requires recreation to remove contents
+		return _subscribers;
+	}
+
+
+	public Channel(string key)
+	{
+		this.key = key;
+	}
+
+
+
+
+	/// <summary>
+	/// send a message to subscribers
+	/// </summary>
+	/// <param name="message"></param>
+	public void Publish(TMessage message)
+	{
+		var _markSubsToClean = false;
+
+		rwLock.EnterReadLock();
+
+		var subs = GetSubscribers();
+
+		try
+		{
+			foreach (var weak in subs)
+			{
+				if (weak.TryGetTarget(out var sub))
+				{
+
+					//foreach (var message in messages)
+					{
+						sub.Enqueue(message);
+					}
+				}
+				else
+				{
+					_markSubsToClean = true;
+				}
+			}
+		}
+		finally
+		{
+			rwLock.ExitReadLock();
+		}
+
+		if (_markSubsToClean == true)
+		{
+			CleanSubs();
+		}
+	}
+
+
+	/// <summary>
+	/// helper to only run the clean code once at a time.
+	/// </summary>
+	private object _cleanLock = new object();
+
+	/// <summary>
+	/// removes any inactive references, and optionally removes a subscriber on-demand.
+	/// </summary>
+	/// <param name="subToRemove"></param>
+	private void CleanSubs(ConcurrentQueue<TMessage> subToRemove = null)
+	{
+		if (Monitor.TryEnter(_cleanLock)) //only 1 clean needs to ever execute at a time.  if others request it, they can skip.
+		{
+			try
+			{
+				//get exclusive access to the list as writing to it is not thread safe
+				rwLock.EnterWriteLock();
+				try
+				{
+
+					//////concurrentBag workflow
+					////var oldSubs = GetSubscribers<V>();
+					////var activeSubs = oldSubs.Where((wr) => wr.TryGetTarget(out var sub) == true && sub != toRemove);
+					////_subscribers = new List<WeakReference<ConcurrentQueue<V>>>(activeSubs);
+
+
+					//list workflow
+					var subs = GetSubscribers();
+					subs.RemoveAll((wr) => wr.TryGetTarget(out var sub) == false || sub == subToRemove);
+
+				}
+				finally
+				{
+					rwLock.ExitWriteLock();
+				}
+			}
+			finally
+			{
+				Monitor.Exit(_cleanLock);
+			}
+		}
+
+	}
+
+	/// <summary>
+	/// register a thread-safe queue that will recieve messages
+	/// </summary>
+	/// <param name="messagesTarget"></param>
+	public void Subscribe(ConcurrentQueue<TMessage> messagesTarget)
+	{
+		rwLock.EnterWriteLock();
+		try
+		{
+			var subs = GetSubscribers();
+			subs.Add(new WeakReference<ConcurrentQueue<TMessage>>(messagesTarget));
+		}
+		finally
+		{
+			rwLock.ExitWriteLock();
+		}
+
+	}
+
+}
--- MockRunner/Program.cs
+++ MockRunner/Program.cs
@@ -82,49 +82,46 @@ Setup(10000);
 ////Allocator.__TEST_Unit_SeriallAllocators();
 ////Allocator.__TEST_Unit_SeriallAllocators();
 
-var start = Stopwatch.StartNew();
+var startedTest = Stopwatch.StartNew();
 
 await Allocator.__TEST_Unit_ParallelAllocators(true,100,externalIdsOwner,1,10000,evenSet,oddSet);
 
-Console.WriteLine($"test elapsed = {start.Elapsed.TotalMilliseconds._Round(2)}ms");
+Console.WriteLine($"test elapsed = {startedTest.Elapsed.TotalMilliseconds._Round(2)}ms");
 
 
 
 //System.Environment.Exit(0);
 
-//var manager = new SimManager() { };
+var manager = new SimManager() { };
 
 
-//var testClass = new MyClass<int>();
-//testClass.GetStorage();
-
-
-
-
-//var start = Stopwatch.GetTimestamp();
-//long lastElapsed = 0;
-
-
-////add some test nodes
-//manager.Register(new TimestepNodeTest { ParentName = "root", Name = "A", TargetFps=1 });
-//manager.Register(new DelayTest { ParentName = "root", Name = "A2" });
-
-//manager.Register(new DelayTest { ParentName = "A", Name = "B", _updateBefore = { "A2" }, _writeResources = {"taco" } });
-//manager.Register(new DelayTest { ParentName = "A", Name = "B2", _readResources = { "taco" } });
-//manager.Register(new DelayTest { ParentName = "A", Name = "B3", _readResources = { "taco" } }); 
-//manager.Register(new DelayTest { ParentName = "A", Name = "B4!", _updateAfter = { "A2" }, _readResources = { "taco" } });
-
-//manager.Register(new DelayTest { ParentName = "B", Name = "C" });
-//manager.Register(new DelayTest { ParentName = "B", Name = "C2" });
-//manager.Register(new DelayTest { ParentName = "B", Name = "C3", _updateAfter = { "C" } });
+var testClass = new MyClass<int>();
+testClass.GetStorage();
 
 
 
 
-//manager.Register(new DebugPrint { ParentName = "C3", Name = "DebugPrint" });
+var start = Stopwatch.GetTimestamp();
+long lastElapsed = 0;
+
+
+//add some test nodes
+manager.Register(new TimestepNodeTest { ParentName = "root", Name = "A", TargetFps = 1 });
+manager.Register(new DelayTest { ParentName = "root", Name = "A2" });
+
+manager.Register(new DelayTest { ParentName = "A", Name = "B", _updateBefore = { "A2" }, _writeResources = { "taco" } });
+manager.Register(new DelayTest { ParentName = "A", Name = "B2", _readResources = { "taco" } });
+manager.Register(new DelayTest { ParentName = "A", Name = "B3", _readResources = { "taco" } });
+manager.Register(new DelayTest { ParentName = "A", Name = "B4!", _updateAfter = { "A2" }, _readResources = { "taco" } });
+
+manager.Register(new DelayTest { ParentName = "B", Name = "C" });
+manager.Register(new DelayTest { ParentName = "B", Name = "C2" });
+manager.Register(new DelayTest { ParentName = "B", Name = "C3", _updateAfter = { "C" } });
 
 
 
+
+manager.Register(new DebugPrint { ParentName = "C3", Name = "DebugPrint" });
 
 
 
@@ -136,78 +133,81 @@ Console.WriteLine($"test elapsed = {start.Elapsed.TotalMilliseconds._Round(2)}ms
 
 
 
-//var loop = 0;
-//while (true)
-//{
-
-//	loop++;
-//	lastElapsed = Stopwatch.GetTimestamp() - start;
-//	start = Stopwatch.GetTimestamp();
-//	//Console.WriteLine($" ======================== {loop} ({Math.Round(TimeSpan.FromTicks(lastElapsed).TotalMilliseconds,1)}ms) ============================================== ");
-//	await manager.Update(TimeSpan.FromTicks(lastElapsed));
-//	//Console.WriteLine($"last Elapsed = {lastElapsed}");
-//}
 
 
 
+var loop = 0;
+while (true)
+{
+
+loop++;
+lastElapsed = Stopwatch.GetTimestamp() - start;
+start = Stopwatch.GetTimestamp();
+//Console.WriteLine($" ======================== {loop} ({Math.Round(TimeSpan.FromTicks(lastElapsed).TotalMilliseconds,1)}ms) ============================================== ");
+await manager.Update(TimeSpan.FromTicks(lastElapsed));
+//Console.WriteLine($"last Elapsed = {lastElapsed}");
+}
 
 
-////[DebuggerVisualizer(typeof(List<T>),)]
-//public class MyClass<T>
-//{
-//	public List<T> GetStorage()
-//	{
-//		//return a list from somewhere...
-//		return new List<T>();
-//	}
-//	private List<T> Storage { get => GetStorage(); }
-//}
 
-//public class DebugPrint : SimNode
-//{
-//	private long avgMs = 0;
-//	protected override async Task Update(Frame frame, NodeFrameState nodeState)
-//	{
-//		//if (frame._stats._frameId % 200 == 0)
-//		{
-//			Console.WriteLine($"{Name} frame {frame} stats={_lastUpdate}");
-//		}		
-		
-//	}
-//}
-//public class TimestepNodeTest : FixedTimestepNode
-//{
-	
-//	protected override async Task Update(Frame frame, NodeFrameState nodeState)
-//	{
-//		await Task.Delay(0);
-//		//Console.WriteLine("WHUT");
-//		//if (frame._stats._frameId % 200 == 0)
-//		{
-//			var indent = GetHierarchy().Count * 3;
-//			Console.WriteLine($"{Name.PadLeft(indent + Name.Length)}");
-//		}
-//		//await Task.Delay(100000);
-//	}
-//}
-//public class DelayTest : SimNode
-//{
-//	private Random _rand = new();
-//	protected override async Task Update(Frame frame, NodeFrameState nodeState)
-//	{
-//		if (Name == "C")
-//		{
-//			Console.WriteLine($"{Name} frame {frame} stats={_lastUpdate}");
-//		}
-//		////Console.WriteLine("WHUT");
-//		if (frame._stats._frameId % 200 == 0)
-//		{
-//			var indent = GetHierarchy().Count * 3;
-			
-//			Console.WriteLine($"{Name.PadLeft(indent + Name.Length)}       START");
-//			//await Task.Delay(_rand.Next(10,100));
-//			//__DEBUG.Assert(false);
-//			Console.WriteLine($"{Name.PadLeft(indent + Name.Length)}       END");
-//		}
-//	}
-//}
+
+
+//[DebuggerVisualizer(typeof(List<T>),)]
+public class MyClass<T>
+{
+	public List<T> GetStorage()
+	{
+		//return a list from somewhere...
+		return new List<T>();
+	}
+	private List<T> Storage { get => GetStorage(); }
+}
+
+public class DebugPrint : SimNode
+{
+	private long avgMs = 0;
+	protected override async Task Update(Frame frame, NodeFrameState nodeState)
+	{
+		//if (frame._stats._frameId % 200 == 0)
+		{
+			Console.WriteLine($"{Name} frame {frame} stats={_lastUpdate}");
+		}
+
+	}
+}
+public class TimestepNodeTest : FixedTimestepNode
+{
+
+	protected override async Task Update(Frame frame, NodeFrameState nodeState)
+	{
+		await Task.Delay(0);
+		//Console.WriteLine("WHUT");
+		//if (frame._stats._frameId % 200 == 0)
+		{
+			var indent = GetHierarchy().Count * 3;
+			Console.WriteLine($"{Name.PadLeft(indent + Name.Length)}");
+		}
+		//await Task.Delay(100000);
+	}
+}
+public class DelayTest : SimNode
+{
+	private Random _rand = new();
+	protected override async Task Update(Frame frame, NodeFrameState nodeState)
+	{
+		if (Name == "C")
+		{
+			Console.WriteLine($"{Name} frame {frame} stats={_lastUpdate}");
+		}
+		////Console.WriteLine("WHUT");
+		if (frame._stats._frameId % 200 == 0)
+		{
+			var indent = GetHierarchy().Count * 3;
+
+			Console.WriteLine($"{Name.PadLeft(indent + Name.Length)}       START");
+			//await Task.Delay(_rand.Next(10,100));
+			//__DEBUG.Assert(false);
+			Console.WriteLine($"{Name.PadLeft(indent + Name.Length)}       END");
+		}
+	}
+}
