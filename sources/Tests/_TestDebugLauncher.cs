5272cff03c9cf97369dd47b4935bb56a3d3bf31b
JasonS <jasons@novaleaf.com>
2021-10-22 14:21:51 -0700

    allow running tests in individual debug

--- sources/Tests/_TestDebugLauncher.cs
+++ sources/Tests/_TestDebugLauncher.cs
@@ -1,26 +1,23 @@
-//// [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] 
-//// [!!] Copyright ©️ NotNot Project and Contributors. 
-//// [!!] By default, this file is licensed to you under the AGPL-3.0.
-//// [!!] However a Private Commercial License is available. 
-//// [!!] See the LICENSE.md file in the project root for more info. 
-//// [!!] ------------------------------------------------- 
-//// [!!] Contributions Guarantee Citizenship! 
-//// [!!] Would you like to know more? https://github.com/NotNotTech/NotNot 
-//// [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!]  [!!] [!!] [!!] [!!]
+// [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] 
+// [!!] Copyright ©️ NotNot Project and Contributors. 
+// [!!] By default, this file is licensed to you under the AGPL-3.0.
+// [!!] However a Private Commercial License is available. 
+// [!!] See the LICENSE.md file in the project root for more info. 
+// [!!] ------------------------------------------------- 
+// [!!] Contributions Guarantee Citizenship! 
+// [!!] Would you like to know more? https://github.com/NotNotTech/NotNot 
+// [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!] [!!]  [!!] [!!] [!!] [!!]
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace Tests
-//{
-//	public static class _TestDebugLauncher
-//	{
-//		public static void Main(params string[] args)
-//		{
-//			Console.WriteLine("HI!");
-//		}
-//	}
-//}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public static class _TestDebugLauncher
+{
+	public static async Task Main(params string[] args)
+	{
+		var test = new Tests.End2End.End2End();
+		await test.CompleteWorkflow();
+	}
+}
